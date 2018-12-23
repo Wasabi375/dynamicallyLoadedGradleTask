1c8b12c054fd4823c0bb4a00b5f4a7ec78901e80
Burkhard Mittelbach <wasabi37a@googlemail.com>
2018-12-23 13:28:50 +0100

    renamed changedFiles to files added sequences for outOfDate and removed
    Signed-off-by: Burkhard Mittelbach <wasabi37a@googlemail.com>

--- interface/src/main/kotlin/wasabi375/dynamicallyLoadedGradleTask/Task.kt
+++ interface/src/main/kotlin/wasabi375/dynamicallyLoadedGradleTask/Task.kt
@@ -7,15 +7,22 @@ abstract class Task (var inputDir: File, var outputDir: File) {
     abstract fun execute(incrementalInput: IncrementalInput)
 }
 
-data class IncrementalInput (val isIncremental: Boolean, val changedFiles: List<FileDetail>) {
+data class IncrementalInput (val isIncremental: Boolean, val files: List<FileDetail>) {
+
+    @Deprecated("renamed to files", ReplaceWith("files"), DeprecationLevel.ERROR)
+    val changedFiles get() = files
 
     fun outOfDate(block: (FileDetail) -> Unit) {
-        changedFiles.asSequence().filter { it.change == ChangeType.Modified || it.change == ChangeType.Added }.forEach(block)
+        outOfDate.forEach(block)
     }
 
+    val outOfDate get() = files.asSequence().filter { it.change == ChangeType.Modified || it.change == ChangeType.Added }
+
     fun removed(block: (FileDetail) -> Unit) {
-        changedFiles.asSequence().filter { it.change == ChangeType.Removed }.forEach(block)
+        removed.forEach(block)
     }
+
+    val removed get() = files.asSequence().filter { it.change == ChangeType.Removed }
 }
 
 data class FileDetail(val file: File, val change: ChangeType)
