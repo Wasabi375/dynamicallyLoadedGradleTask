80d9d4a5b0e076639bf11d98043562d89ca70ece
Burkhard Mittelbach <wasabi37a@googlemail.com>
2019-02-26 17:23:23 +0100

    rework to use command line args instead, not finished
    Signed-off-by: Burkhard Mittelbach <wasabi37a@googlemail.com>

--- Readme.md
+++ Readme.md
@@ -4,49 +4,18 @@
 
 A gradle task that let's you run a program and delegate the incremental task execution to it. 
 
-## Startup
+The file information is send via the program arguments:
 
-The communication with the program is done using the standard in and out channels. 
-At the start the program gets all data about the files in the following way:
+* number of files total
+* number of Modified
+* modified file paths
+* number of Added
+* added file paths
+* number of Removed
+* removed file paths
+* number of unchanged (should be 0 with current system)
+* unchanged file paths
 
-| Master | |
-| --- | --- |
-| incremental ||
-| true *or* false ||
-| input dir ||
-| \<input dir> ||
-| output dir ||
-| \<output dir> ||
-| total count|  |
-| \<total file count> | |
-| total modified | |
-| \<total modified count> ||
-| total added ||
-| \<total added count> ||
-| total removed ||
-| \<total removed count> ||
-| total unchanged ||
-| \<total unchanged count> ||
-
-After that client can ask for each of those file categories. The following client messages are by the client are answered
-by the master
-
-| Client | Master | |
-| --- | --- | --- |
-| all | a list of all file names one per line | \<change type>: \<filename> |
-| all modified | a list of all modified files on per line | \<filename> |
-| all added | a list of all added files on per line | \<filename> |
-| all removed | a list of all removed files on per line | \<filename> |
-| all unchanged | a list of all unchanged files on per line | \<filename> |
-| | | |
-| next | the next file or empty line | \<filename> *or* \<newline> |
-| next modified | the next modified file or empty line | \<filename> *or* \<newline> |
-| next added | the next added file or empty line | \<filename> *or* \<newline> |
-| next removed | the next removed file or empty line | \<filename> *or* \<newline> |
-| next unchanged | the next unchanged file or empty line | \<filename> *or* \<newline> |
-
-*next* commands and *all* commands are not exclusive. *all* commands will always send all files, even if *next*  
-has been used.  
 
 ## Result
 
@@ -55,10 +24,4 @@ the gradle task fails.
 
 ## Logging
 
-Anything written to the standard err stream will be logged as an error. In order to achieve different logging levels
-the first message send to the client should be the log level `{ trace, info, warn, error }` followed in a new line by the message.
-If the message is multiple lines long, the log level should be followed by the line count, e.g `warn 3\n...`.
-
-## New Line
-
-The new line character should be `\n`
+Anything printed to std-out and std-err streams will just be delegated to the gradle std-out and std-err streams.
--- task/src/main/kotlin/wasabi375/dynamicgradletask/DynamicallyLoadedGradleTask.kt
+++ task/src/main/kotlin/wasabi375/dynamicgradletask/DynamicallyLoadedGradleTask.kt
@@ -4,13 +4,11 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.*
 import org.gradle.api.tasks.incremental.IncrementalTaskInputs
 import java.io.*
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.io.BufferedWriter
-import java.lang.StringBuilder
+import java.util.concurrent.TimeUnit
 
+@Suppress("unused")
 open class DynamicallyLoadedGradleTask : DefaultTask() {
 
     lateinit var target: String
@@ -25,52 +23,55 @@ open class DynamicallyLoadedGradleTask : DefaultTask() {
     @OutputDirectory
     lateinit var outputDir: File
 
-    private var logRestAndFinish = false
-
     private val logger: Logger = LoggerFactory.getLogger("Dynamic Task Runner")!!
-    var programLogger: Logger = LoggerFactory.getLogger("Dynamic Task Program")!!
 
     private lateinit var data: IncrementalInput
 
+    @Volatile
+    var running = true
 
     @TaskAction
     fun execute(inputs: IncrementalTaskInputs) {
 
         data = convertInputs(inputs)
 
-        val process = startProgram()
+        val args = createArgs(data)
+        val process = startProgram(args)
+
+        val outThread = thread("output stream logging") {
+            val error = process.errorStream
+            while(running) {
+                for (i in 0 until error.available()) {
+                    System.out.println("" + error.read())
+                }
+                Thread.sleep(10)
+            }
+        }
+        val errThread = thread("error stream logging") {
+            val out = process.inputStream
+            while(running) {
+                for (i in 0 until out.available()) {
+                    System.out.println("" + out.read())
+                }
+                Thread.sleep(10)
+            }
+        }
+
         try {
+            outThread.run()
+            errThread.run()
 
-            val handleErrors = GlobalScope.launch {
-                process.handleErrors()
-            }
+            process.waitFor()
 
-            process.sendStartupData()
-
-            val handleInputs = GlobalScope.launch {
-                process.handleInput()
-            }
-
-
-            try {
-                val result = process.waitFor()
-                if (process.exitValue() != 0 || result != 0) {
-                    throw Exception("Sub program failed with error code $result")
-                }
-            } finally {
-                logRestAndFinish = true
-                runBlocking {
-                    withTimeout(5000) {
-                        handleErrors.join()
-                        handleInputs.join()
-                    }
-                }
-            }
         } finally {
             if(process.isAlive) {
                 logger.error("Program did not terminate. It will be killed...")
                 process.destroy()
             }
+            process.waitFor(5, TimeUnit.SECONDS)
+            running = false
+            outThread.join()
+            errThread.join()
         }
     }
 
@@ -101,7 +102,27 @@ open class DynamicallyLoadedGradleTask : DefaultTask() {
         else -> ChangeType.NoChange
     }
 
-    private fun startProgram(): Process {
+    private fun createArgs(input: IncrementalInput): List<String> {
+        val args = mutableListOf<String>()
+
+        args.add(input.files.size.toString())
+
+        args.add(input.modifiedCount.toString())
+        args.addAll(input.modified.map { it.file.absolutePath })
+
+        args.add(input.addedCount.toString())
+        args.addAll(input.added.map { it.file.absolutePath })
+
+        args.add(input.removedCount.toString())
+        args.addAll(input.removed.map { it.file.absolutePath })
+
+        args.add(input.unchangedCount.toString())
+        args.addAll(input.unchanged.map { it.file.absolutePath })
+
+        return args
+    }
+
+    private fun startProgram(args: List<String>): Process {
 
         if (runInJava) {
             val targetFile = File(target)
@@ -117,150 +138,6 @@ open class DynamicallyLoadedGradleTask : DefaultTask() {
 
         return Runtime.getRuntime().exec(command)
     }
-
-    private suspend fun Process.handleErrors() {
-
-        val channel = errorStream.toLineChannel(this@DynamicallyLoadedGradleTask::logRestAndFinish)
-        channel.consumeEach {
-            programLogger.error(it)
-        }
-    }
-
-    private suspend fun Process.handleInput() {
-
-        val channel = inputStream.toLineChannel(this@DynamicallyLoadedGradleTask::logRestAndFinish)
-        val writer = outputStream.bufferedWriter()
-
-        while(true) {
-            val line = channel.receive()
-            logger.debug("received: $line")
-            if(line.isBlank()) {
-                logger.warn("received empty line")
-                continue
-            }
-
-            val parts = line.split(" ")
-            if(parts.size > 2) {
-                logger.error("received invalid line: $line")
-            }
-
-            when(parts[0]) {
-                // Logging
-                "trace" -> log(channel, parts[0], parts.getOrNull(1))
-                "info" -> log(channel, parts[0], parts.getOrNull(1))
-                "warn" -> log(channel, parts[0], parts.getOrNull(1))
-                "error" -> log(channel, parts[0], parts.getOrNull(1))
-
-                // data request
-                "all" -> sendAll(writer, parts.getOrNull(1))
-                "next" -> sendNext(writer, parts.getOrNull(1))
-
-                else -> logger.error("invalid command received: $line")
-            }
-        }
-    }
-
-    private fun sendAll(writer: BufferedWriter, argument: String?) {
-
-        if(logRestAndFinish) {
-            logger.warn("request \"all ${argument ?: ""}\" after finish!")
-            return
-        }
-        when(argument) {
-            null -> data.files.asSequence().forEach { writer.sendln(it) }
-            "modified" -> data.modified.map { it.file.absolutePath }.forEach { writer.sendln(it) }
-            "added" -> data.added.map { it.file.absolutePath }.forEach { writer.sendln(it) }
-            "removed" -> data.removed.map { it.file.absolutePath }.forEach { writer.sendln(it) }
-            "unchanged" -> data.unchanged.map { it.file.absolutePath }.forEach { writer.sendln(it) }
-            else -> logger.error("invalid command received: all $argument")
-        }
-    }
-
-    private val nextFileSeq by lazy { data.files.iterator() }
-    private val nextModifiedSeq by lazy { data.modified.map { it.file.absolutePath }.iterator() }
-    private val nextAddedSeq by lazy { data.added.map { it.file.absolutePath }.iterator() }
-    private val nextRemovedSeq by lazy { data.removed.map { it.file.absolutePath }.iterator() }
-    private val nextUnchangedSeq by lazy { data.unchanged.map { it.file.absolutePath }.iterator() }
-
-    private fun sendNext(writer: BufferedWriter, argument: String?) {
-        if(logRestAndFinish) {
-            logger.warn("request \"next ${argument ?: ""}\" after finish!")
-            return
-        }
-
-        when(argument) {
-            null -> writer.sendln(if(nextFileSeq.hasNext()) nextFileSeq.next() else null)
-            "modified" -> writer.sendln(if(nextModifiedSeq.hasNext()) nextModifiedSeq.next() else "")
-            "added" -> writer.sendln(if(nextAddedSeq.hasNext()) nextAddedSeq.next() else "")
-            "removed" -> writer.sendln(if(nextRemovedSeq.hasNext()) nextRemovedSeq.next() else "")
-            "unchanged" -> writer.sendln(if(nextUnchangedSeq.hasNext()) nextUnchangedSeq.next() else "")
-            else -> logger.error("invalid command received: all $argument")
-        }
-    }
-
-    private suspend fun log(channel: ReceiveChannel<String>, level: String, lineCountText: String?) {
-
-        val lineCount = if(lineCountText == null) 1 else {
-            lineCountText.toIntOrNull() ?: kotlin.run {
-                logger.error("invalid line count for logging received: $lineCountText")
-                return
-            }
-        }
-        val textBuilder = StringBuilder()
-        repeat(lineCount) {
-            textBuilder.appendln(channel.receive())
-        }
-        val text = textBuilder.toString()
-
-        when(level){
-            "trace" -> programLogger.debug(text)
-            "info" -> programLogger.info(text)
-            "warn" -> programLogger.warn(text)
-            "error" -> programLogger.error(text)
-        }
-    }
-
-    private fun Process.sendStartupData() {
-
-        with(outputStream.bufferedWriter()) {
-
-            sendln("incremental")
-            sendln(data.isIncremental)
-
-            sendln("input dir")
-            sendln(inputDir.absolutePath)
-            sendln("output dir")
-            sendln(outputDir.absolutePath)
-
-            sendln("total count")
-            sendln(data.files.size)
-
-            sendln("total modified")
-            sendln(data.modifiedCount)
-
-            sendln("total added")
-            sendln(data.addedCount)
-
-            sendln("total removed")
-            sendln(data.removedCount)
-
-            sendln("total unchanged")
-            sendln(data.unchangedCount)
-        }
-    }
-
-    private fun BufferedWriter.sendln(s: String) {
-        logger.debug("Send data: $s")
-        appendln(s)
-    }
-    private fun BufferedWriter.sendln(i: Int) = sendln(i.toString())
-    private fun BufferedWriter.sendln(b: Boolean) = sendln(b.toString())
-
-    private fun BufferedWriter.sendln(f: FileDetail?) {
-        if(f == null) sendln("")
-        else {
-            val data = "${f.change.stringRepresentation}:${f.file.absolutePath}"
-            sendln(data)
-        }
-    }
 }
+
+private fun thread(name: String, code: () -> Unit): Thread = Thread(code, name)
